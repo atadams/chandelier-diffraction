5859f2fddd9f503e495360f6e55f78c900f51a1c
Tony Adams <atadams@gmail.com>
2024-01-15 13:57:49 -0600

    Code cleanup

--- chandelier.py
+++ chandelier.py
@@ -1,7 +1,6 @@
-from diffractsim import ApertureFromImage, Lens, MonochromaticField, PolychromaticField, mm, cm, cf, nm, set_backend
+from diffractsim import ApertureFromImage, Lens, PolychromaticField, cf, cm, mm, set_backend
 
 set_backend("CPU")  # Change the string to "CUDA" to use GPU acceleration
-
 
 F = PolychromaticField(
     spectrum=1.5 * cf.illuminant_d65,
@@ -15,8 +14,9 @@ F.add(ApertureFromImage("./apertures/mts-stagger-2.png", image_size=(20 * mm, 20
 
 F.add(Lens(f=45 * cm))
 F.propagate(35 * cm)
+
 F.add(ApertureFromImage("./apertures/square.png", image_size=(5 * mm, 5 * mm), simulation=F))
 F.propagate(10 * cm)
 
 rgb = F.get_colors()
-F.plot_colors(rgb, xlim=[-6 * mm, 6 * mm], ylim=[-6 * mm, 6 * mm], figsize=(10, 10))
+F.plot_colors(rgb, xlim=[-4 * mm, 4 * mm], ylim=[-4 * mm, 4 * mm], figsize=(10, 10))
